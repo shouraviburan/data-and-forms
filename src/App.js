cabb2b7e04f359f9e14380384bb273128b171ba6
BULENT KAYICI <bulentkyc@MacBook-Pro.fritz.box>
2021-03-17 13:36:12 +0100

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [countryList, setCountryList] = useState([])
+
+  let url = 'https://restcountries.eu/rest/v2/all';
+  let options = {
+    method:"GET"
+  };
+
+  useEffect(()=>{
+    fetch(url, options).then(result => {
+          console.log(result);
+          result.json().then(output => {
+            console.log(output);
+            //Code goes here...
+            setCountryList(output.map(item=><li>{item.name}</li>));
+          })
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>{countryList}</ul>
       </header>
     </div>
   );
