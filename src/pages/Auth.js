a536f5412ccd133cb202cb1f3e91dc17cc9e7074
BULENT KAYICI <bulentkyc@MacBook-Pro.fritz.box>
2021-03-18 14:21:30 +0100

    auth

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -4,15 +4,42 @@ export default function() {
 
     let submitHandler = (e) => {
         e.preventDefault();
+        console.log(e);
+
+        let data = {};
+
+        data.email = e.target[0].value;
+        data.pass = e.target[1].value;
+
+        let url = 'https://auth404.herokuapp.com/api/auth/register';
+        let options = {
+            method:'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body:JSON.stringify(data)
+        }
+
+        if (e.nativeEvent.submitter.id == "register"){
+            //alert('register')
+            //console.log(data)
+            fetch(url, options).then(result=>result.json().then(output=>console.log(output)));
+        } else {
+            alert('else')
+            //fetch('https://auth404.herokuapp.com/api/auth/login');
+        }
     }
 
     return (
         <div className = "wrapper">
             <form onSubmit = {submitHandler}>
-                <input type = "email" />
-                <input type = "password" />
-                <input type = "submit" value = "Register"/>
-                <input type = "submit" value = "Login"/>
+                <input name = "email" type = "email" />
+                <input  name = "pass" type = "password" />
+                <section>
+                    <input id = "register" type = "submit" value = "Register"/>
+                    <input id = "login" type = "submit" value = "Login"/>
+                </section>
             </form>
         </div>
     )
